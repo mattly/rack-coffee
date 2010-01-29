1b5103e289489daf634e6c939a5a2d23de802301
Matt Lyon <matt@flowerpowered.com>
2010-01-28 23:15:33 -0800

    remove dependency on Pathname, instead using File. Oh, if only Pathname were stdlib instead of extlib.

--- lib/rack/coffee.rb
+++ lib/rack/coffee.rb
@@ -1,4 +1,3 @@
-require 'pathname'
 require 'time'
 require 'coffee-script'
 require 'rack/file'
@@ -6,29 +5,27 @@ require 'rack/utils'
 
 module Rack
   class Coffee
+    F = ::File
     
     attr_accessor :url, :root
     
     def initialize(app, opts={})
       @app = app
       @url = opts[:url] || '/javascripts'
-      @root = Pathname.new(opts[:root] || Dir.pwd)
+      @root = opts[:root] || Dir.pwd
       @server = Rack::File.new(root)
     end
     
     def call(env)
       path = Utils.unescape(env["PATH_INFO"])
       return [403, {"Content-Type" => "text/plain"}, ["Forbidden\n"]] if path.include?('..')
-      if (path.index(url) == 0) and (path =~ /\.js$/)
-        coffee = root + path.sub(/\.js$/,'.coffee').sub(/^\//,'')
-        if coffee.file?
-          headers = {"Content-Type" => "application/javascript", "Last-Modified" => coffee.mtime.httpdate}
-          [200, headers, [CoffeeScript.compile(coffee.read)]]
-        else
-          @server.call(env)
-        end
+      return @app.call(env) unless (path.index(url) == 0) and (path =~ /\.js$/)
+      coffee = F.join(root, path.sub(/\.js$/,'.coffee'))
+      if F.file?(coffee)
+        headers = {"Content-Type" => "application/javascript", "Last-Modified" => F.mtime(coffee).httpdate}
+        [200, headers, [CoffeeScript.compile(F.read(coffee))]]
       else
-        @app.call(env)
+        @server.call(env)
       end
     end
     
